6dbe90b7e7b30834b5e6e827ca485092a340e8ce
Suba-1985 <134758157+Suba-1985@users.noreply.github.com>
2023-07-11 13:43:33 -0400

    runner comit

--- src/test/java/dsAlgo_TestRunner/TestRunner.java
+++ src/test/java/dsAlgo_TestRunner/TestRunner.java
@@ -1,31 +1,50 @@
 package dsAlgo_TestRunner;
-import org.testng.annotations.DataProvider;
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-
-@io.cucumber.testng.CucumberOptions(
-		//plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
-				plugin = { "pretty",
-						"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-						"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
-						"json:target/cucumber-report.json"},
-		monochrome=false,  //console output color
-		 //tags from feature file
-		features = {"src/test/resources/Features"},
-		//tags = "@invalidpythoncode",	
-		//tags={"@Login"}, //location of feature files (should be in src/test/reso
-		glue={"dsAlgo_StepDefinition","hooks"})//location of  step definition files
-		public class TestRunner  extends AbstractTestNGCucumberTests{
-			
-			@Override
-		    @DataProvider(parallel = false)
-		    public Object[][] scenarios() {
-						
-				return super.scenarios();
-		    }
-		}	
+//@io.cucumber.testng.CucumberOptions(
+//		//plugin = {"pretty", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}, //reporting purpose
+//				plugin = { "pretty",
+//						"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+//						"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+//						"json:target/cucumber-report.json"},
+//		monochrome=false,  //console output color
+//		 //tags from feature file
+//		features = {"src/test/resources/Features"},
+//		//tags = "@invalidpythoncode",	
+//		//tags={"@Login"}, //location of feature files (should be in src/test/reso
+//		glue={"dsAlgo_StepDefinition","hooks"})//location of  step definition files
+//		public class TestRunner  extends AbstractTestNGCucumberTests{
+//			
+//			@Override
+//		    @DataProvider(parallel = false)
+//		    public Object[][] scenarios() {
+//						
+//				return super.scenarios();
+//		    }
+//		}	
 	
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions; 
+
+@RunWith(Cucumber.class) 
+@CucumberOptions(  
 	
+//Specifying pretty as a format option ensure that HTML report will be generated. 
+//When we specify html:target/Destination - It will generate the HTML report 
+
+//inside the Destination folder, in the target folder of the maven project. 
+plugin = { "pretty",
+		"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+		"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
+		"json:target/cucumber-report.json"},
+         monochrome=false,  //console output color
+////tags from feature file
+features = {"src/test/resources/Features"},
+////tags = "@invalidpythoncode",	
+////tags={"@Login"}, //location of feature files (should be in src/test/reso
+glue={"dsAlgo_StepDefinition","hooks"})//location of  step definition files
+
+public class TestRunner { }
 	
 	
 	
